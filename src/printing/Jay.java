0351dcc34894d45e9cad7045a7d2cfa78d4f841d
Jay Wu <wu274319426@gmail.com>
2017-06-19 14:19:59 -0400

    debug assignment

--- src/printing/Jay.java
+++ src/printing/Jay.java
@@ -25,6 +25,32 @@ public class Jay {
 		int y = 1986;
 	    System.out.println("Jay's age");
 		System.out.println(x-y);	
+		
+		
+		
+		
+		
+		/* Debug assignment*/
+		package printing;
+
+		public class Debug {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				double x, y;
+				
+				x = 3.1415;
+				y = 3.64;
+				
+				System.out.println("pi is approximately " +x);
+				System.out.println("My GPA was" +y);
+				
+			}
+
+		}
+
 	}
 
 }
+
+
