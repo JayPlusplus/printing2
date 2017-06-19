680316b7f962acb3374b8336d2654829059ad9cf
Jay Wu <wu274319426@gmail.com>
2017-06-19 14:15:12 -0400

    debug assignment

---
+++ src/printing/Debug.java
@@ -0,0 +1,17 @@
+package printing;
+
+public class Debug {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x, y;
+		
+		x = 3.1415;
+		y = 3.64;
+		
+		System.out.println("pi is approximately " +x);
+		System.out.println("My GPA was" +y);
+		
+	}
+
+}
